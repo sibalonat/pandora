06ee6a043beb53b612485fca22f0e9e11c2da185
Marin Nikolli <sibalonat@gmail.com>
2022-10-15 14:26:13 +0200

    global styles

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,8 +1,7 @@
 import { css, SerializedStyles } from "@emotion/react";
 import styled from "@emotion/styled";
 import { MouseEvent } from "react";
-// css
-// Mouse
+import { AppTheme } from "@/styles/themes";
 
 export type Color = "primary" | "secondary" | "danger" | "warning";
 
@@ -12,26 +11,22 @@ export type Props = {
     onClick: (event: MouseEvent<HTMLButtonElement>) => void;
 };
 
-export const getColors = (color?: Color): SerializedStyles => {
+export const getColors = (theme: AppTheme, color?: Color): SerializedStyles => {
     switch (color) {
         case "secondary":
             return css `
-            background: #5e5c64e6;
+            color: ${theme.fonts.regular};
             `;
+        case "primary":
         case "danger":
             return css `
-            background: #dc3545e6;
-            color: #e4ebf5e6;
+            background: ${theme.components[color]};
+            color: ${theme.fonts.button};
             `;
         case "warning":
             return css `
-            background: #ffca2ce6;
-            color: #e4ebf5e6;
-            `;
-        case "primary":
-            return css `
-            background: #6d5dfc;
-            color: #e4ebf5e6;
+            background: ${theme.components[color]};
+            color: ${theme.fonts.warning};
             `;
         default:
             return css ``;
@@ -51,13 +46,13 @@ export const Button = styled.button<Props>`
     height: 4rem;
     border-radius: 1rem;
     transition: all .4s ease;
-    ${({color}) => getColors(color)}
-    ${({ theme }) => `box-shadow: 1vmin 1vmin 1vmin ${theme.components.shadow1}, -1vmin -1vmin 1vmin ${theme.components.shadow1}`}
+    ${({theme, color}) => getColors(theme, color)}
+    ${({ theme }) => `box-shadow: 1vmin 1vmin 1vmin ${theme.components.shadow1}, -1vmin -1vmin 1vmin ${theme.components.shadow1}`};
     &:hover { 
         opacity: .4;
     }
     &:active {
-        box-shadow: 1vmin 1vmin 1vmin #c8d0e7 inset, -1vmin -1vmin 1vmin #ffffff inset;
+        ${({ theme }) => `box-shadow: 1vmin 1vmin 1vmin ${theme.components.shadow1} inset, -1vmin -1vmin 1vmin ${theme.components.shadow1} inset`};
     }
 `;
 
