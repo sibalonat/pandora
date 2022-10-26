0dca808464cb97921275ecb2ac46851c2bb01845
Marin Nikolli <sibalonat@gmail.com>
2022-10-26 12:59:38 +0200

    geting the data from strapi

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,11 @@
-import { Note } from '@/components/Notes/Notes'
-import styled from '@emotion/styled'
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import { Note } from "@/components/Notes/Notes";
+import styled from "@emotion/styled";
+import type { GetStaticProps, NextPage } from "next";
+import Head from "next/head";
 // import Image from 'next/image'
 // Note
 // styled
+import { Note as NoteType, Response } from "@/types";
 
 const NoteWrapper = styled.div`
   display: flex;
@@ -12,44 +13,72 @@ const NoteWrapper = styled.div`
   gap: 2vw;
   margin: 2vw 1vw;
   width: 100vw;
-`
+`;
 
-const Home: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>mnplus</title>
-        <meta name='description' content='created by generating a nextap' />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <NoteWrapper>
-        {Array(4)
-        .fill("")
+type NoteResponse = Response<NoteType[]>;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const api_url = process.env.NEXT_PUBLIC_STRAPI_API_URL;
+  const response = await fetch(`${api_url}/notes?populate=*`, {
+    method: "GET",
+  });
+  const { data: notes, meta, error }: NoteResponse = await response.json();
+
+  const status = error?.status;
+  if (status && (status < 200 || status >= 300)) {
+    return {
+      props: {
+        notes: [],
+        meta: {},
+      },
+    };
+  }
+  return {
+    props: {
+      notes,
+      meta: meta,
+    },
+  };
+};
+
+const Home: NextPage<{
+  notes: NoteType[];
+}> = ({ notes }) => (
+  <>
+    <Head>
+      <title>mnplus</title>
+      <meta name="description" content="created by generating a nextap" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <NoteWrapper>
+      {notes
         .map(() => (
-           <Note
-           key={Math.random()}
-           header='marini po degjon balthazar'
-           link='/balthazar'
-           imageProps={{
-             width: 1368,
-             height: 770,
-             alt: "random foto",
-             src: "https://picsum.photos/seed/picsum/1368/770"
-           }}
-         >
-           <>
-             React is a weird library for javascript when people get overexited for something it can have more sense.
-             <ul>
-               <li>first what is react</li>
-               <li>reason to switch to vue</li>
-               <li>things that meaby will make you switch that sometimes i fell </li>
-             </ul>
-           </>
-         </Note>
+          <Note
+            key={Math.random()}
+            header="marini po degjon balthazar"
+            link="/balthazar"
+            imageProps={{
+              width: 1368,
+              height: 770,
+              alt: "random foto",
+              src: "https://picsum.photos/seed/picsum/1368/770",
+            }}
+          >
+            <>
+              React is a weird library for javascript when people get overexited
+              for something it can have more sense.
+              <ul>
+                <li>first what is react</li>
+                <li>reason to switch to vue</li>
+                <li>
+                  things that meaby will make you switch that sometimes i fell{" "}
+                </li>
+              </ul>
+            </>
+          </Note>
         ))}
-      </NoteWrapper>
-    </>
-  )
-}
+    </NoteWrapper>
+  </>
+);
 
-export default Home
+export default Home;
